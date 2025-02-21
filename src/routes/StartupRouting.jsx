89a655b60d55ed0424a7fb2e5571352585cf5084
shailu0007 <shailendradhakad314@gmail.com>
2025-02-21 12:33:18 +0530

    startup directory

--- src/routes/StartupRouting.jsx
+++ src/routes/StartupRouting.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Profile from '../components/startup/Profile'
 import Dashboard from '../components/startup/Dashboard'
+import Startup_directory from '../components/startup/Startup_directory'
 
 const Startup = () => {
   return (
     <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/startup-directory" element={<Startup_directory />} />
     </Routes>
   )
 }
