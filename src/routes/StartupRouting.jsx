83a4751ada5cd1cb448e89d028cfb23eafea62c2
Mohit-Yyadav <mohityadav8338@gmail.com>
2025-03-05 16:48:43 +0530

    add startupForm

--- src/routes/StartupRouting.jsx
+++ src/routes/StartupRouting.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Profile from "../components/startup/Profile";
 import Dashboard from "../components/startup/Dashboard";
+import Startupdirective from "../components/startup/Startup_directory";
+import StartupProfileForm from "../components/startup/StartupForm";
 
 const Startup = () => {
   return (
     <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/startup-directory" element={<Startup_directory />} />
+        <Route path="/startup-directory" element={<Startupdirective />} />
+        <Route path="/startup-Form" element={<StartupProfileForm/>} />
     </Routes>
   )
 }
